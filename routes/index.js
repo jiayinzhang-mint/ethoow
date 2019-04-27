09da6b9b2b661aa77ebf211fe07680d759e4e39a
MINT <mint@insdim.com>
2019-04-27 23:43:22 +0800

    fuck

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,32 @@ router.get('/dive', (req, res, next) => {
   res.render('dive');
 });
 
+router.get('/para', (req, res, next) => {
+  res.render('para');
+});
+
+router.get('/selffly', (req, res, next) => {
+  res.render('selffly');
+});
+
+router.get('/hunt', (req, res, next) => {
+  res.render('hunt');
+});
+
+router.get('/fish', (req, res, next) => {
+  res.render('fish');
+});
+
+router.get('/waterdive', (req, res, next) => {
+  res.render('waterdive');
+});
+
+router.get('/taobao', (req, res, next) => {
+  res.render('taobao');
+});
+
+router.get('/about', (req, res, next) => {
+  res.render('about');
+});
+
 module.exports = router;
